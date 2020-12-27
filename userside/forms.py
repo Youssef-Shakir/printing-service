0d1a31c6a39ef8830eb3fbc75c24eabf5bb28d83
youssef shakir <youssef.shakir.2003@gmail.com>
2020-12-27 16:18:21 +0300

    laptop

--- userside/forms.py
+++ userside/forms.py
@@ -1,7 +1,12 @@
 from django import forms
 from .models import order
-
+from phonenumber_field.formfields import PhoneNumberField
+from PyPDF2 import PdfFileReader
 class CreateOrderForm(forms.ModelForm):
-	class Meta:
-		model = order
-		fields = '__all__'
+    class Meta:
+        model = order
+        widgets = {
+        'paper_size' : forms.RadioSelect(),
+        'location' : forms.TextInput(attrs={'placeholder':'ksamnk'})
+        }
+        fields = ['pdf_file','paper_size','bending_type','phone_number','location','notes']
